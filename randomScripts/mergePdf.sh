12e6a49eb79366d4ba5030ed47b535d42248f40c
Janet Mardjuki <janet.mardjuki@gmail.com>
2023-03-31 00:59:07 -0700

    Added to do

--- randomScripts/mergePdf.sh
+++ randomScripts/mergePdf.sh
@@ -5,6 +5,8 @@
 MERGERAPP=pdfunite
 OUTTEXT=out.pdf
 
+# TODO: Follow the readarray way to deal with spaces
+# https://unix.stackexchange.com/questions/9496/looping-through-files-with-spaces-in-the-names
 for FILE in $(ls | grep .pdf)
 do
 	if test -f "$FILE"; then
